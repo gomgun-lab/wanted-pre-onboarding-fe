ae47968ffd5916db5b234dc06888764da9aa5433
gomgun <zxolqs@gmail.com>
2022-08-24 08:21:28 +0900

    Feat : NotFound 페이지 구현

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Card from "../components/common/Card";
+
+const NotFound = () => {
+  return (
+    <Card>
+      <h1>잘못된 페이지 경로입니다...</h1>
+    </Card>
+  );
+};
+
+export default NotFound;
